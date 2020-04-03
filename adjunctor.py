f946dbc8a00afb576495c3d142a8e37ade261b70
scotchtape22 <scottwambold@gmail.com>
2020-04-03 09:32:14 -0400

    Update and rename broker.py to adjunctor.py

---
+++ adjunctor.py
@@ -0,0 +1,26 @@
+#!/bin/python
+# Copper and Coal Adjunctor
+# Using a save file and a list of orders - Perform the next part of the game
+# Returns a new save file
+# Will ideally be used with the draw functions to create the map
+
+# Spy Phase
+# Policy Changes
+# Purifier Ejections
+# Spy Missions
+
+# Power Phase
+# LoS Update
+# Defenses are Updated
+# Queue Production
+# Queue Trades
+
+# Maneuver Phase
+# Assign Priority Random Numbers to prevent ties
+# Surface Moves
+# Each step
+
+# Production Phase
+# Run for each settlement
+
+# Resolution Phase
