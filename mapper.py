69cead44dfb72c8fbd1dc1c9ef9b2f2de8d4c665
scotchtape22 <scottwambold@gmail.com>
2020-06-04 09:58:32 -0400

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -1,66 +1,22 @@
 #!/bin/python
 
-# Hex object
+# Minimal Options for start:
+# Players - 4,6,8
+# Game Clock - 10 Minutes vs 24 Hours
+# Game Length - Short (12 turns or 3 years - 500 Terraform), Medium (20 turns or 5 years - 1000 Terraform), Long (40 turns or 10 years - 2000 Terraform)
+# Anonymous - (Y/N)
 
-# 
+# Metadata Set Up
+# Game Options
 
-def __make_map__(options):
-	# Requiers a dictionary of information
-	# Returns a map and game settings as an object
+# Create Map Hexes
 
-	# Set information from dictionary:
-	game_speed = options['g_speed']
-	day_skip = options['d_skip']
-	colony_limit = options['c_limit']
-	comm_type = options['c_type']
-	
+# Create Capitals based on player count
+# Create elysium (1-1-1 "capital" in the center)
 
-	
+# Fill out the rest of the provinces
+# Choose a hex and a mix of resouces/infastructure
+# Check that it keeps the pie slice in balance - else choose new hex
+# Apply hex and name
 
-	
-	
-	# Create hexes based on player count
-	# Determine limits for neutral cities
-	# 4,6,8 players
-	
-	n_ports = 0
-	n_purifiers = 0
-	n_foundaries = 0
-	y_limit = 0
-	
-	if options['p_count'] == 4:
-	elif options['p_count'] == 6:
-	elif options['p_count'] == 8:
-	else:
-		error("Invalid player count.")
-
-	# Create empty grid
-	y = 0
-	y_now = 0
-	x = 0
-	
-	while y_now < y_limit:
-	
-
-
-	# Create prime hex
-	# Always Elysium port
-
-	# Create player home bases
-	if options['p_count'] == 4:
-	elif options['p_count'] == 6:
-	elif options['p_count'] == 8:
-	else:
-		error("Invalid player count.")
-	
-	
-	# Fill neutrals
-	# Ports first
-	while n_ports > 0:
-		# Pick spot
-		
-	# Then Purifiers
-	# Then Foundaries
-	
-	# Build object
-	
+# Create Save File
