30a7957983732d5721490dd73d75801ee50f3cd7
scotchtape22 <scottwambold@gmail.com>
2020-06-04 12:07:26 -0400

    Update classes.py

--- classes.py
+++ classes.py
@@ -7,12 +7,28 @@ import random
 class grid_hex:
   def __init__(self):
     # Grid ID
+    self.grid_id = grid_id
     # Adjacent Grid IDs
-    # Settlement Same
+    self.adjacencies = adjacencies
+    # Settlement Name
+    self.title = title
     # Resource Type
+    self.type = type
     # Settlement is capital?
-    # Settlement infastructure
+    self.capital = is_capital
+    # Settlement infrastructure
+    self.infra = infra
     # Automaton Counts (8 different Nations)
+    self.n1a = n1a
+    self.n2a = n2a
+    self.n3a = n3a
+    self.n4a = n4a
+    self.n5a = n5a
+    self.n6a = n6a
+    self.n7a = n7a
+    self.n8a = n8a
+    #Calamity
+    self.calamity = calamity
 
 # Nation Object
 class nation:
